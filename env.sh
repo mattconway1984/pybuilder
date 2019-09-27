f9e0ba83a73d017c48443f816851c24e20adb112
Matthew Conway <matthew.denis.conway@gmail.com>
2019-09-27 10:15:20 +0100

    Remove un-necessary stuff from Dockerfile & tidy up env.sh

--- env.sh
+++ env.sh
@@ -6,50 +6,32 @@ set -o pipefail
 ARGS="$@"
 BUILDER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR=$( cd $BUILDER_DIR/../ && pwd)
-IMAGE_NAME=$(docker run --rm -i -v $PROJECT_DIR:/tmp python:3.6-slim /bin/bash -c "cd /tmp && python setup.py --name")
-
-# If no artifactory credentials are supplied, then setup some rubbish ones to
-# allow developers to run "make wheel/test" locally etc. Won't be able to
-# publish, but that's okay because only Jenkins should publish artifacts.
-if [ -z "$ARTIFACTS_USER" ]; then
-    ARTIFACTS_USER="nobody"
-fi
-if [ -z "$ARTIFACTS_TOKEN" ]; then
-    ARTIFACTS_TOKEN="empty"
-fi
-if [ -z "$ARTIFACTORY_INDEX" ]; then
-    ARTIFACTORY_INDEX="none"
-fi
-
-# If no SAMBA credentials are supplied, then setup some rubbish ones to allow
-# developers to run "make wheel/test" locally etc. Won't be able to publish docs
-# locally, but that's okay because only Jenkins should publish docs.
-if [ -z "$SMB_USER" ]; then
-    SMB_USER="nobody"
-fi
-if [ -z "$SMB_PASSWORD" ]; then
-    SMB_PASSWORD="none"
-fi
+# To get the name of the current project, run 'python setup.py --name'
+# inside a container (the build server may not have python installed).
+PROJECT_NAME=$(
+    docker run --rm -i -v $PROJECT_DIR:/tmp python:3.6-slim \
+    /bin/bash -c "cd /tmp && python setup.py --name")
 
 set -u
 
-docker build $BUILDER_DIR -t $IMAGE_NAME
+# Build the docker container (using the Dockerfile from pybuilder)
+docker build $BUILDER_DIR -t $PROJECT_NAME
 
-# If no args are passed, just start the docker container
+# If no args are passed, just start the docker container (gives bash prompt)
 if [ -z "$ARGS" ]; then
 docker run \
-    --env PROJECT_NAME=$IMAGE_NAME \
+    --env PROJECT_NAME=$PROJECT_NAME \
     --rm \
     -v $PROJECT_DIR:/workspace \
-    -it --entrypoint /bin/bash $IMAGE_NAME
+    -it \
+    --entrypoint /bin/bash \
+    $PROJECT_NAME
 else
 docker run \
-    --env TWINE_USERNAME=$ARTIFACTS_USER \
-    --env TWINE_PASSWORD=$ARTIFACTS_TOKEN \
-    --env TWINE_REPOSITORY_URL=$ARTIFACTORY_INDEX \
-    --env SAMBA_USERNAME=$SMB_USER \
-    --env SAMBA_PASSWORD=$SMB_PASSWORD \
-    --env PROJECT_NAME=$IMAGE_NAME \
-    --rm -i -v $PROJECT_DIR:/workspace \
-    $IMAGE_NAME /bin/bash -c "cd /workspace/builder && $ARGS"
+    --env PROJECT_NAME=$PROJECT_NAME \
+    --rm \
+    -i \
+    -v $PROJECT_DIR:/workspace \
+    $PROJECT_NAME \
+    /bin/bash -c "cd /workspace/pybuilder && $ARGS"
 fi
